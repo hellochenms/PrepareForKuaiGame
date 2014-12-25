9f4c4b7cc3325d2e1a83fcbd7eb81fcd501e3892
iami10buxi <iami10buxi@sina.com>
2014-12-25 18:06:38 +0800

    @

---
+++ PrepareForKuaiGame/VideoViewController.h
@@ -0,0 +1,13 @@
+//
+//  VideoViewController.h
+//  PrepareForKuaiGame
+//
+//  Created by thatsoul on 14/12/25.
+//  Copyright (c) 2014年 chenms.m2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VideoViewController : UIViewController
+
+@end
